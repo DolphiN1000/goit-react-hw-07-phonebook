0db8a5f8b8c5ac99c2fe4a1e3c0ffd550ea67e08
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-03-19 23:02:09 +0200

    fix all

--- src/components/Phonebook/ContactList/ContactList.jsx
+++ src/components/Phonebook/ContactList/ContactList.jsx
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 import ContactsListItem from './ContactsListItem/ContactsListItem';
 
 import styles from './contactList.module.scss';
+import { useSelector } from 'react-redux';
 
+import { getFilteredContacts } from 'redux/phonebook/phonebook-selectors';
 
-const ContactsList = ({ contacts, deleteContact }) => {
+const ContactsList = () => {
+  const filteredContacts = useSelector(getFilteredContacts);
+
   // const contactsSorted = contacts;
   // console.log(contacts)
   // .sort(function (a, b) {
@@ -18,16 +22,8 @@ const ContactsList = ({ contacts, deleteContact }) => {
   //   return 0;
   // });
 
-  const elements = contacts.map(({ id, name, number }) => {
-    return (
-      <ContactsListItem
-        key={id}
-        id={id}
-        name={name}
-        number={number}
-        deleteContact={deleteContact}
-      />
-    );
+  const elements = filteredContacts.map(({ id, name, number }) => {
+    return <ContactsListItem key={id} id={id} name={name} number={number} />;
   });
 
   return <ol className={styles.list}>{elements}</ol>;
@@ -47,5 +43,4 @@ ContactsList.propTypes = {
       number: PropTypes.string.isRequired,
     })
   ),
-  deleteContact: PropTypes.func.isRequired,
 };
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,55 +1,33 @@
 import '../../shared/styles/styles.scss';
-
-import ContactsForm from './ContactForm/ContactForm';
-import Filter from './Filter/Filter';
-
-
 import { lazy, Suspense } from 'react';
+
 import { FidgetSpinner } from 'react-loader-spinner';
 
-import {
-  fetchAddContact,
-  fetchAllContacts,
-  fetchDeleteContact,
-} from 'redux/phonebook/phonebook-operations';
+import { fetchAllContacts } from 'redux/phonebook/phonebook-operations';
 
-import {
-    getFilteredContacts,
-} from 'redux/phonebook/phonebook-selectors';
+import { getFilteredContacts } from 'redux/phonebook/phonebook-selectors';
 
 import styles from './phonebook.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-
-import { setFilter } from 'redux/filter/filter-slice';
+const ContactsForm = lazy(() => import('./ContactForm/ContactForm'));
+const Filter = lazy(() => import('./Filter/Filter'));
 const ContactsList = lazy(() => import('./ContactList/ContactList'));
+
 const Phonebook = () => {
   const filteredContacts = useSelector(getFilteredContacts);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchAllContacts());
   }, [dispatch]);
 
-  const handleAddContact = ({ name, number }) => {
-    dispatch(fetchAddContact({ name, number }));
-  };
-
-  const handleDeleteContact = id => {
-    dispatch(fetchDeleteContact(id));
-  };
-
-  const handleFilter = ({ target }) => {
-    dispatch(setFilter(target.value));
-  };
-
   const isContacts = Boolean(filteredContacts.length);
 
   return (
     <Suspense
       fallback={
-        <FidgetSpinner
+           <FidgetSpinner
           visible={true}
           height="200"
           width="200"
@@ -60,19 +38,14 @@ const Phonebook = () => {
           backgroundColor="#F4442E"
         />
       }
-    >
-      <div className={styles.container}>
+      >
+         <div className={styles.container}>
         <h1>Phonebook</h1>
 
-        <ContactsForm onSubmit={handleAddContact} />
+        <ContactsForm />
         <h2>Contacts</h2>
-        <Filter handleChange={handleFilter} />
-        {isContacts && (
-          <ContactsList
-            contacts={filteredContacts}
-            deleteContact={handleDeleteContact}
-          />
-        )}
+        <Filter />
+        {isContacts && <ContactsList contacts={filteredContacts} />}
         {!isContacts && <p>Not yet added contacts</p>}
       </div>
     </Suspense>
